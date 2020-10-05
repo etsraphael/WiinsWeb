aac4fbf682debb1419191f3cccfdac392c6f3a66
etsraphael <ets_raphael@outlook.fr>
2020-10-05 14:51:23 +1000

    minor update

--- src/app/core/models/comment/comment-publication.model.ts
+++ src/app/core/models/comment/comment-publication.model.ts
@@ -2,13 +2,13 @@ import { CommentModel } from './comment.model';
 
 export class CommentFeedPublication extends CommentModel {
 
-  idPublication: string;
+  publicationId: string;
   publicationProfile: string;
   commentProfile: string;
 
-  constructor(tagFriend: string[], text: string, baseComment: string, commentProfile: string, idPublication: string, publicationProfile: string ) {
+  constructor(tagFriend: string[], text: string, baseComment: string, commentProfile: string, publicationId: string, publicationProfile: string ) {
     super(tagFriend, text, baseComment, commentProfile, 'feed-publication');
-    this.idPublication = idPublication;
+    this.publicationId = publicationId;
     this.publicationProfile = publicationProfile;
     this.baseComment = baseComment;
     this.commentProfile = commentProfile;
