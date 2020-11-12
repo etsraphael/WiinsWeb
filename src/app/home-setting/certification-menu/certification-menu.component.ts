96296993844ccddcae23b54d7175a77736da1979
etsraphael <ets_raphael@outlook.fr>
2020-11-12 11:05:37 +1000

    minors update

--- src/app/home-setting/certification-menu/certification-menu.component.ts
+++ src/app/home-setting/certification-menu/certification-menu.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { filter, skipWhile } from 'rxjs/operators';
+import { ProfileModel } from 'src/app/core/models/baseUser/profile.model';
+import { ProfileFeatureStoreSelectors, RootStoreState } from 'src/app/root-store';
 
 @Component({
   selector: 'app-certification-menu',
   templateUrl: './certification-menu.component.html',
   styleUrls: ['./certification-menu.component.scss']
 })
+
 export class CertificationMenuComponent implements OnInit {
 
-  constructor() { }
+  // profile 
+  myprofile$: Observable<ProfileModel>
+
+  constructor(
+    private store$: Store<RootStoreState.State>
+  ) { }
 
   ngOnInit() {
+
+    // to select my profile
+    this.myprofile$ = this.store$.pipe(
+      select(ProfileFeatureStoreSelectors.selectProfile),
+      skipWhile(val => val === null),
+      filter(profile => !!profile),
+    )
+    
   }
 
 }
