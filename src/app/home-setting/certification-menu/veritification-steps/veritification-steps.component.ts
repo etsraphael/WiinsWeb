cdcd1f72d3c448374f18148637ecc823e322b83f
etsraphael <ets_raphael@outlook.fr>
2020-11-14 18:58:45 +1000

    send both of the file

--- src/app/home-setting/certification-menu/veritification-steps/veritification-steps.component.ts
+++ src/app/home-setting/certification-menu/veritification-steps/veritification-steps.component.ts
@@ -1,5 +1,5 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCheckboxChange, MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
@@ -16,13 +16,15 @@ import * as uuid from 'uuid'
   styleUrls: ['./veritification-steps.component.scss']
 })
 
-export class VeritificationStepsComponent implements OnInit {
+export class VeritificationStepsComponent implements OnInit, OnDestroy {
 
   // file
   fileIdname: string
   pictureTakeName: string
+  fileIdLink: string
   fileId: File
   pictureTake: File
+  pictureTakeLink: string
 
   // sub
   uploadSubFileId: Subscription
@@ -32,6 +34,7 @@ export class VeritificationStepsComponent implements OnInit {
 
   // service 
   requestPending: boolean = false
+  loading: boolean = false
 
   constructor(
     private translate: TranslateService,
@@ -52,44 +55,15 @@ export class VeritificationStepsComponent implements OnInit {
   }
 
   saveIdFile(files: any) {
-
-    // // to print the name of the file
-    // if (event.target.files.length == 0) return null
-    // this.fileIdname = event.target.files[0].name
-
-    // // to compress to a file
-    // this.uploadService.urltoFile(
-    //   event.base64,
-    //   event.target.files[0].name,
-    //   event.target.files[0].type
-    // ).then(file => { this.fileId = file })
-
-    // // upload the first file
-    // const reader = new FileReader()
-    // reader.onloadend = _event => {
-    //   console.log('ca passe')
-    //   // this.uploadFile(environment.link_verification, this.fileId)
-    // }
-
-    // reader.readAsDataURL(this.fileId)
-
     if (files.length === 0) return null
     this.fileIdname = files[0].name
-    const reader = new FileReader()
-
-    // compress the file
-    reader.onloadend = _event => {
-      this.uploadFile(environment.link_verification, files[0])
-    }
-
-    // read the file
-    reader.readAsDataURL(files[0])
-
+    this.fileId = files[0]
   }
 
-  savepictureTakeFile(event: any) {
-    if (event.target.files.length == 0) return null
-    this.pictureTakeName = event.target.files[0].name
+  savepictureTakeFile(files: any) {
+    if (files.length == 0) return null
+    this.pictureTakeName = files[0].name
+    this.pictureTake = files[0]
   }
 
   changeCheckBtn(event: MatCheckboxChange) {
@@ -97,18 +71,34 @@ export class VeritificationStepsComponent implements OnInit {
   }
 
   confirm(): void | MatSnackBarRef<SimpleSnackBar> {
-    return this._snackBar.open(
-      this.translate.instant('ERROR-MESSAGE.Els-are-incorrects'), null,
-      { horizontalPosition: 'center', verticalPosition: 'bottom', duration: 5000 }
-    )
+
+    if (!this.fileIdname || !this.pictureTakeName) {
+      return this._snackBar.open(
+        this.translate.instant('ERROR-MESSAGE.Els-are-incorrects'), null,
+        { horizontalPosition: 'center', verticalPosition: 'bottom', duration: 5000 }
+      )
+    }
+
+    this.loading = true
+
+    // send the fileId
+    const reader = new FileReader()
+    reader.onloadend = _event => { this.uploadFile(this.fileId, 'fileId') }
+    reader.readAsDataURL(this.fileId)
+
+    // send the pictureTake and send the verification
+    const reader2 = new FileReader()
+    reader2.onloadend = _event => { this.uploadFile(this.pictureTake, 'pictureTake') }
+    reader2.readAsDataURL(this.pictureTake)
+    
   }
 
   // to upload a file
-  uploadFile(bucketName: string, file: File) {
+  uploadFile(file: File, type: string) {
 
     // create the object to get the signed url from the backend
     const urlSigned: UrlSigned = {
-      Bucket: bucketName,
+      Bucket: environment.link_verification,
       Key: uuid.v4(),
       ContentType: file.type
     }
@@ -118,7 +108,7 @@ export class VeritificationStepsComponent implements OnInit {
       (response: RespondGetUploadUrl) => {
         // upload to s3
         return this.uploadService.uploadfileAWSS3(response.url, file).subscribe(
-          (response: HttpEvent<{}>) => this.updateProgress(response, urlSigned),
+          (response: HttpEvent<{}>) => this.updateProgress(response, urlSigned, type),
           (error: any) => console.log(error))
       },
       (error: RespondGetUploadUrl) => console.log(error)
@@ -128,22 +118,44 @@ export class VeritificationStepsComponent implements OnInit {
   }
 
   // to update the loading bar
-  updateProgress(event: HttpEvent<{}>, urlSigned: UrlSigned): void {
+  updateProgress(event: HttpEvent<{}>, urlSigned: UrlSigned, type: string): void {
     switch (event.type) {
-      case HttpEventType.Response: { this.sendTheConfirmation(urlSigned.Bucket, urlSigned.Key); break; }
+      case HttpEventType.Response: { this.sendTheConfirmation(urlSigned.Bucket, urlSigned.Key, type); break; }
       default: break
     }
   }
 
-    // send the confirmation
-    sendTheConfirmation(bucketName: string, key: string): void {
+  // update the url
+  sendTheConfirmation(bucketName: string, key: string, type: string): void {
 
-      // to get the url
-      const url = this.uploadService.getFileUrlAfterUpload(bucketName, key)
-      console.log(url)
-  
-  
+    switch (type) {
+      case 'fileId':
+        this.fileIdLink = this.uploadService.getFileUrlAfterUpload(bucketName, key)
+        break;
+      case 'pictureTake':
+        this.pictureTakeLink = this.uploadService.getFileUrlAfterUpload(bucketName, key)
+        this.sendVerification()
+        break;
     }
+  }
+
+  sendVerification(): void | MatSnackBarRef<SimpleSnackBar> {
+    
+      if (!this.pictureTakeLink || !this.fileIdLink) {
+        return this._snackBar.open(
+          this.translate.instant('ERROR-MESSAGE.A-err-has-occurred'), null,
+          { horizontalPosition: 'center', verticalPosition: 'bottom', duration: 5000 }
+        )
+      }
+  
+      this.loading = false
+      this.requestPending = true
+  }
+
+  ngOnDestroy(): void {
+    if(this.uploadSubFileId) this.uploadSubFileId.unsubscribe()
+  }
+
 }
 
 export interface VerificationForm {
