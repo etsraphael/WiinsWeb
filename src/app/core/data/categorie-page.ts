1b6ad023217dfb85e591f4972f116355df7d069f
etsraphael <ets_raphael@outlook.fr>
2020-11-18 19:24:37 +1000

    categorie page data added

---
+++ src/app/core/data/categorie-page.ts
@@ -0,0 +1,91 @@
+export const categoriePage = [
+    {
+        code: 1,
+        groupTitle: 'Pro',
+        Description: '(Entreprise/ institution/ Association/ Organisation)',
+        subGroup: [
+            { code: 1, name: 'Compagny' },
+            { code: 2, name: 'Association' },
+            { code: 3, name: 'Institution' },
+            { code: 4, name: 'Organization' }
+        ]
+    },
+    {
+        code: 2,
+        groupTitle: 'NoPro',
+        subGroup: [
+            { code: 1, name: 'Food' },
+            { code: 2, name: 'Automobile' },
+            { code: 3, name: 'DIY' },
+            { code: 4, name: 'Cultur' },
+            { code: 5, name: 'Humor' },
+            { code: 6, name: 'Information' },
+            { code: 7, name: 'Health' },
+            { code: 7, name: 'Science' },
+            { code: 8, name: 'Sport' },
+            { code: 9, name: 'Technology' }
+        ]
+    },
+    {
+        code: 3,
+        groupTitle: 'Pro/NoPro',
+        subGroup: [
+            {
+                code: 1, name: 'Musician',
+                subGroup: [
+                    { code: 1, name: 'Composer' },
+                    { code: 2, name: 'Group' },
+                    { code: 3, name: 'Interpreter' }
+                ]
+            },
+            {
+                code: 2, name: 'Videographer',
+                subGroup: [
+                    { code: 1, name: 'Director' },
+                    { code: 2, name: 'Filmmaker' },
+                    { code: 3, name: 'Editor' },
+                    { code: 4, name: 'Actor' }
+                ]
+            },
+            {
+                code: 3, name: 'Image',
+                subGroup: [
+                    { code: 1, name: 'Photographer' },
+                    { code: 2, name: 'Painter' }
+                ]
+            },
+            {
+                code: 4, name: 'Writing',
+                subGroup: [
+                    { code: 1, name: 'Writer' },
+                    { code: 2, name: 'Humorist' },
+                    { code: 3, name: 'Author' },
+                    { code: 4, name: 'Journalist' }
+                ]
+            },
+            {
+                code: 4, name: 'Support',
+                subGroup: [
+                    { code: 1, name: 'Editor' },
+                    { code: 2, name: 'Record company' },
+                    { code: 3, name: 'Studio' }
+                ]
+            },
+            {
+                code: 4, name: 'Athletic',
+                subGroup: [
+                    { code: 1, name: 'Editor' },
+                    { code: 2, name: 'Record company' },
+                    { code: 3, name: 'Studio' }
+                ]
+            },
+            {
+                code: 4, name: 'Other',
+                subGroup: [
+                    { code: 1, name: 'Influencer' },
+                    { code: 4, name: 'Other' }
+                ]
+            },
+        ]
+    }
+]
