dff702f5a23cc7c1193e45f353d10c7c69d55c6c
etsraphael <ets_raphael@outlook.fr>
2021-02-14 11:19:44 +1000

    fix - display messenger card on little screen

--- src/app/core/component/footer-core/footer-core.component.ts
+++ src/app/core/component/footer-core/footer-core.component.ts
@@ -84,8 +84,7 @@ export class FooterCoreComponent implements OnInit {
       backdropClass: '.no-backdrop',
       height: '27.5rem',
       width: '21rem',
-      // position: { left: position.x + 30 + 'px', top: position.y - 50 + 'px' },
-      position: { left: offsetLeft + 'px', bottom: '4.5rem' },
+      position: { left: offsetLeft + 'px', bottom: '4%' },
       data: { cardHeader: room.participants, currentRoom: room, myProfileID: this.user.profile, searching: false }
     });
 
