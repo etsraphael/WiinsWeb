009e67c6130b28d2ed00db5b3757715be1c64685
etsraphael <ets_raphael@outlook.fr>
2021-03-03 18:08:13 +1000

    fix - size of windows fixed

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const { app, BrowserWindow } = require("electron")
-const path = require("path")
-const url = require("url")
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
 let win
 
 function createWindow() {
 
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({show: false});
+  win.maximize();
+  win.show();
 
   // load the dist folder from Angular
   win.loadURL(
