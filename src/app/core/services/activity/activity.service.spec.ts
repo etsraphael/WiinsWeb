88c87f9c96f0ab04301cec6b605222a728b444ef
etsraphael <ets_raphael@outlook.fr>
2021-05-03 17:07:14 +1000

    ActivityService updated

--- src/app/core/services/activity/activity.service.spec.ts
+++ src/app/core/services/activity/activity.service.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ActivityService } from './activity.service';
 
 describe('ActivityService', () => {
+  let service: ActivityService;
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ActivityService = TestBed.get(ActivityService);
     expect(service).toBeTruthy();
   });
 });
