a548eb738410ea406f5e137a7176e199e206b7e9
etsraphael <ets_raphael@outlook.fr>
2021-06-28 20:23:22 +1000

    transition is working

--- src/assets/route-animation/sign-animation.ts
+++ src/assets/route-animation/sign-animation.ts
@@ -1,47 +1,59 @@
-import { animate, group, query, style, transition, trigger, useAnimation } from '@angular/animations'
-import { jello, lightSpeedIn } from 'ngx-animate'
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+} from "@angular/animations";
+import { jello, lightSpeedIn } from "ngx-animate";
 
-export const SignAnimation =[
-  trigger('routeAnimations', [
-    transition('SignIn => OnBoarding', [
-      query(':enter, :leave',
-        style({ }),
-        { optional: true }),
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(-200%)'}),
-          animate('0.6s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-        query(':leave', [
-          style({ transform: 'translateX(0%)' }),
-          animate('0.6s ease-in-out',
-            style({ transform: 'translateX(100%)' }))
-        ], { optional: true }),
-      ])
+export const SignAnimation = [
+  trigger("routeAnimations", [
+    transition("SignIn => OnBoarding", slideTo("left")),
+    transition("OnBoarding => SignIn", slideTo("right")),
+  ]),
+  trigger("stateLogin", [
+    transition(
+      "default => error",
+      useAnimation(jello, { params: { timing: 1.2 } })
+    ),
+  ]),
+  trigger("donation", [
+    transition(
+      "* => true",
+      useAnimation(lightSpeedIn, { params: { timing: 0.5 } })
+    ),
+  ]),
+];
+
+function slideTo(direction: string) {
+  const optional = { optional: true };
+  return [
+    query(
+      ":enter, :leave",
+      [
+        style({
+          position: "absolute",
+          top: 0,
+          [direction]: 0,
+          width: "100%",
+        }),
+      ],
+      optional
+    ),
+    query(":enter", [style({ [direction]: "-100%" })]),
+    group([
+      query(
+        ":leave",
+        [animate("600ms ease", style({ [direction]: "200%", opacity: 0 }))],
+        optional
+      ),
+      query(":enter", [animate("600ms ease", style({ [direction]: "0%" }))]),
     ]),
-    transition('OnBoarding => SignIn', [
-      query(':enter, :leave',
-        style({ }),
-        { optional: true }),
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(100%)'}),
-          animate('0.6s ease-in-out',
-            style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-        query(':leave', [
-          style({ transform: 'translateX(0%)' }),
-          animate('0.6s ease-in-out',
-            style({ transform: 'translateX(-200%)' }))
-        ], { optional: true }),
-      ])
-    ])
-  ]),
-  trigger('stateLogin', [
-    transition('default => error', useAnimation(jello,{ params: { timing: 1.2 }}))
-  ]),
-  trigger('donation', [
-    transition('* => true', useAnimation(lightSpeedIn,{ params: { timing: 0.5 }}))
-  ])
-]
+    query(":leave", animateChild()),
+    query(":enter", animateChild()),
+  ];
+}
