2300ade70ace3a99944beef215dca99ff48c3e8f
etsraphael <ets_raphael@outlook.fr>
2021-07-21 20:15:35 +1000

    cleaning code

--- src/app/sign/to-stripe/to-stripe.component.ts
+++ src/app/sign/to-stripe/to-stripe.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { SignAnimation } from 'src/assets/route-animation/sign-animation';
+import { Component, OnInit } from "@angular/core";
+import { SignAnimation } from "src/assets/route-animation/sign-animation";
 
 @Component({
-  selector: 'app-to-stripe',
-  templateUrl: './to-stripe.component.html',
-  styleUrls: ['./to-stripe.component.scss'],
-  animations: [SignAnimation]
+  selector: "app-to-stripe",
+  templateUrl: "./to-stripe.component.html",
+  styleUrls: ["./to-stripe.component.scss"],
+  animations: [SignAnimation],
 })
 export class ToStripeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
