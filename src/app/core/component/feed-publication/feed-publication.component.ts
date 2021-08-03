7e6c22736d6d3f00f45f150c9b00ef38a16ed558
etsraphael <ets_raphael@outlook.fr>
2021-08-03 21:23:58 +1000

    ngx-image-compress updated

--- src/app/core/component/feed-publication/feed-publication.component.ts
+++ src/app/core/component/feed-publication/feed-publication.component.ts
@@ -451,7 +451,7 @@ export class FeedPublicationComponent implements OnInit, OnDestroy {
   }
 
   // to upload a file and compress
-  uploadFileAndCompress(bucketName: string, file: File, reader: ArrayBuffer | String) {
+  uploadFileAndCompress(bucketName: string, file: File, reader: ArrayBuffer | String | any) {
 
     // create the object to get the signed url from the backend
     const urlSigned: UrlSigned = {
